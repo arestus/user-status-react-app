e518b7ac4007fab94cb18b6b56d3580b71566988
arestus <arestus@gmail.com>
2021-05-30 22:00:24 +0300

    Added form settings

--- src/components/FormMain/FormMain.js
+++ src/components/FormMain/FormMain.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import cities from '../../cities.json';
 
 import styles from './FormMain.module.css';
 
@@ -19,34 +20,53 @@ const FormMain = () => {
   return (
     <>
       <h1>Здравствуйте, Человек №3596941</h1>
+
       <a href="#" onClick={() => setInput(!input)}>
         Изменить статус
       </a>
+
       {input && (
         <form onSubmit={handleChangeStatus}>
           <input type="text" value={value} onChange={handleChange} />
           <button type="submit">Сменить статус</button>
         </form>
       )}
+
       <p>{status}</p>
+
       <div className={styles.container__form}>
         <form className={styles.form}>
-          <label>
-            Пароль
-            <input />
-          </label>
-          <label>
-            Пароль еще раз
-            <input />
-          </label>
-          <label>
-            Электронная почта
-            <input />
-          </label>
-          <label>
-            Я согласен
+          <select className={styles.form__select}>
+            {cities.map((option, i) => (
+              <option value={option.city} key={i}>
+                {option.city}
+              </option>
+            ))}
+          </select>
+          <div className={styles.form__field}>
+            <label>Пароль</label>
+            <input type="text" />
+            <span>Ваш новый пароль должен содержать не менее 5 символов.</span>
+          </div>
+          <div className={styles.form__field}>
+            <label>Пароль еще раз</label>
+            <input type="text" />
+            <span>
+              Повторите пароль, пожалуйста, это обезопасит вас с нами на случай
+              ошибки.
+            </span>
+          </div>
+          <div className={styles.form__field}>
+            <label>Электронная почта</label>
+            <input type="text" />
+            <span>Можно изменить адрес, указанный при регистрации.</span>
+          </div>
+          <div className={styles.form__field}>
+            <label>Я согласен</label>
             <input type="checkbox" />
-          </label>
+            <span>принимать актуальную информацию на емейл</span>
+          </div>
+          <button type="submit">Изменить</button>
         </form>
       </div>
     </>
